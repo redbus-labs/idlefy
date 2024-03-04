a125d68cb61895df6ceeef7e0a53ed5d0fa0c4a1
Harsh Kumar Choudhary <harsh.choudhary@HARSH-CHOUDHARY-MAC.local>
2024-03-05 00:43:57 +0530

    feat: adds defineIdleProperty helper class

---
+++ src/defineIdleProperties.ts
@@ -0,0 +1,16 @@
+import { defineIdleProperty } from './defineIdleProperty'
+
+export function defineIdleProperties<T, K extends PropertyKey>( // K for Key type
+  obj: Record<K, any>,
+  props: Record<K, () => T>,
+): void {
+  Object.keys(props).forEach((propAsString) => {
+    const prop = propAsString as K // Type assertion
+    try {
+      defineIdleProperty(obj, prop, props[prop])
+    }
+    catch (error) {
+      console.error(`Error defining idle property '${propAsString}':`, error)
+    }
+  })
+}
