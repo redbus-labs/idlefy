42a2a3f64be19a716f5ad8dc908d0293a07cc5b4
Harsh Choudhary <harsh.hkc1006@gmail.com>
2024-03-05 00:45:58 +0530

    feat: adds defineIdleProperty helper class (#9)
    * chore: adds more strict type checking for idleValue class
    
    * feat: adds defineIdleProperty helper class
    
    ---------
    
    Co-authored-by: Harsh Kumar Choudhary <harsh.choudhary@HARSH-CHOUDHARY-MAC.local>

---
+++ src/defineIdleProperty.ts
@@ -0,0 +1,21 @@
+import { IdleValue } from './IdleValue'
+
+export function defineIdleProperty<T, TInitFunc extends () => T, K extends PropertyKey>( // Add K
+  obj: Record<K, any>,
+  prop: K,
+  init: TInitFunc,
+): void {
+  if (!obj || typeof obj !== 'object')
+    throw new TypeError('obj must be an object')
+
+  if (!prop)
+    throw new Error('prop must be provided')
+
+  const idleValue = new IdleValue<T, TInitFunc>(init)
+
+  Object.defineProperty(obj, prop, {
+    configurable: true,
+    get: idleValue.getValue.bind(idleValue),
+    set: idleValue.setValue.bind(idleValue),
+  })
+}
